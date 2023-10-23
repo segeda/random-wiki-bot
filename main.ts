f951c3fa2402589696c12814d2d150482a0f2824
Petr Severa <segedacz@gmail.com>
2023-10-23 15:21:43 +0200

    add action and main script

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+const resp = await fetch("https://cs.wikipedia.org/wiki/Special:Random");
+const html = await resp.text();
+const document = new DOMParser().parseFromString(html, "text/html");
+const script = document.querySelector('script[type="application/ld+json"]');
+const jsonld = JSON.parse(script.textContent);
+
+console.log(jsonld.url, jsonld.name);
